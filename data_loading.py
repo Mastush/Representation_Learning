16720efb609fb8bc37ef5a379f8097e2017129f5
Mastuh <mastush2@gmail.com>
2020-03-28 17:47:06 +0300

    complete general vector dataset (with pca) and mnist dataset

--- data_loading.py
+++ data_loading.py
@@ -1,5 +1,9 @@
 import numpy as np
 from abc import abstractmethod
+from sklearn.datasets import fetch_openml
+from sklearn.decomposition import PCA
+
+import utils
 
 
 class RepresentableDataset:
@@ -7,21 +11,68 @@ class RepresentableDataset:
         self._representations = representations
 
     @abstractmethod
-    def get_training_examples(self, n=None, apply_representations=True):
+    def get_training_examples(self, n: int = None, apply_representations: bool = True):
         """ If n is None, then this should return an entire dataset """
         pass
 
-    def apply_representations_to_example(self, example):
+    def apply_representations(self, x):
         for rep in self._representations:
-            example = rep(example)
-        return example
+            x = rep(x)
+        return x
 
 
-class RepresentableMnist(RepresentableDataset):
-    def __init__(self, train_path: str, test_path: str, representations: list):
-        super(RepresentableMnist, self).__init__(representations)
-        self._train_path = train_path
-        self._test_path = test_path
+class RepresentableVectorDataset(RepresentableDataset):
+    def __init__(self, representations: list):
+        super(RepresentableVectorDataset, self).__init__(representations)
+        self._PCA = None
 
-    def get_training_examples(self, n=None, apply_representations=True):
+    def _apply_pca(self, x, d: int=None):
+        if d is not None and self._PCA is None:
+            self._PCA = PCA(d)
+            self._PCA.fit(x, None)
+        return self._PCA.transform(x)
+
+    @abstractmethod
+    def get_training_examples(self, n: int = None, apply_representations: bool = True,
+                              dim_reduction: int = None, shuffle: bool = False):
         pass
+
+    @abstractmethod
+    def get_test_examples(self, n: int = None, apply_representations: bool = True,
+                          dim_reduction: int = None, shuffle: bool = False):
+        pass
+
+    def get_examples(self, x, y, n: int = None, apply_representations: bool = True,
+                     dim_reduction: int = None, shuffle: bool = False):
+        if dim_reduction is not None:
+            x = self._apply_pca(x, dim_reduction)
+        else:
+            x = np.copy(x)
+        y = np.copy(y)
+        if shuffle:
+            x, y = utils.unison_shuffled_copies(x, y)
+        if apply_representations:
+            x = self.apply_representations(x)
+        return x, y if n is None else x[:n], y[:n]
+
+
+class RepresentableMnist(RepresentableVectorDataset):
+    def __init__(self, representations: list):
+        super(RepresentableMnist, self).__init__(representations)
+        x, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+        self._training_ims = x[:60000]
+        self._training_labels = y[:60000]
+        self._test_ims = x[60000:]
+        self._test_labels = y[:60000:]
+        self._PCA = None
+
+    def get_training_examples(self, n: int = None, apply_representations: bool = True,
+                              dim_reduction: int = None, shuffle: bool = False):
+        return self.get_examples(self._training_ims, self._training_labels, n, apply_representations,
+                                 dim_reduction, shuffle)
+
+    def get_test_examples(self, n: int = None, apply_representations: bool = True,
+                          dim_reduction: int = None, shuffle: bool = False):
+        return self.get_examples(self._test_ims, self._test_labels, n, apply_representations,
+                                 dim_reduction, shuffle)
+
--- utils.py
+++ utils.py
@@ -5,5 +5,12 @@ import numpy as np
 def get_device():
     return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 def safe_tensor_to_ndarray(tensor):
     return np.copy(tensor.data.cpu().numpy())
+
+
+def unison_shuffled_copies(a, b):
+    assert len(a) == len(b)
+    p = np.random.permutation(len(a))
+    return a[p], b[p]
